6551fcdaaafbdf4e1b42f0ab85a9869bae6172e7
Tuyen Huynh <47501532+httuyen@users.noreply.github.com>
2019-09-17 19:33:39 +0700

    cm

--- src/main/java/com/ui/FrmMain.java
+++ src/main/java/com/ui/FrmMain.java
@@ -9,10 +9,19 @@ import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.GlyphMetrics;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
@@ -20,8 +29,10 @@ import org.eclipse.swt.widgets.ToolItem;
 public class FrmMain {
 
 	protected Shell shell;
+
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
@@ -53,37 +64,78 @@ public class FrmMain {
 	protected void createContents() {
 		shell = new Shell();
 		shell.setLayout(new GridLayout(1, false));
-		
-		
+
 		ToolBar toolBar = new ToolBar(shell, SWT.FLAT | SWT.RIGHT);
 		toolBar.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
 		
-		ToolItem tltmNewItem = new ToolItem(toolBar, SWT.NONE);
-		tltmNewItem.setText("File");
+		// item file
+		ToolItem itemFile = new ToolItem(toolBar, SWT.DROP_DOWN);
+		itemFile.setText("File");
 		
-		ToolItem tltmNewItem_1 = new ToolItem(toolBar, SWT.NONE);
+		Menu fileMenu = new Menu(toolBar);
+		MenuItem openFile = new MenuItem(fileMenu, SWT.PUSH);
+		openFile.setText("&Open\tCTRL+O");
+		openFile.setAccelerator(SWT.CTRL + 'O');
+		
+		
+		class Open implements SelectionListener{
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fd = new FileDialog(shell, SWT.OPEN);
+		        fd.setText("Open");
+		        fd.setFilterPath("C:/");
+		        String[] filterExt = { "*.txt", "*.doc", ".rtf", "*.*", "*.xml" };
+		        fd.setFilterExtensions(filterExt);
+		        fd.open();
+		        //String selected = fd.open();
+			}
+
+			@Override
+			public void widgetDefaultSelected(SelectionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		}
+		openFile.addSelectionListener(new Open());
+		itemFile.addListener(SWT.Selection, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				if (event.detail == SWT.ARROW) {
+					Rectangle rect = itemFile.getBounds();
+					Point pt = new Point(rect.x, rect.y + rect.height);
+					pt = toolBar.toDisplay(pt);
+					fileMenu.setLocation(pt.x, pt.y);
+					fileMenu.setVisible(true);
+				}
+			}
+		});
+
+		ToolItem tltmNewItem_1 = new ToolItem(toolBar, SWT.DROP_DOWN);
 		tltmNewItem_1.setText("Search");
-		
+
 		final StyledText styledText = new StyledText(shell, SWT.BORDER | SWT.V_SCROLL | SWT.MULTI | SWT.H_SCROLL);
 		styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, true, 1, 1));
-		
+
 		styledText.addLineStyleListener(new LineStyleListener() {
-		    @Override
-		    public void lineGetStyle(LineStyleEvent event) {
-		        // Using ST.BULLET_NUMBER sometimes results in weird alignment.
-		        //event.bulletIndex = styledText.getLineAtOffset(event.lineOffset);
-		        StyleRange styleRange = new StyleRange();
-		        styleRange.foreground = Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
-		        int maxLine = styledText.getLineCount();
-		        int bulletLength = Integer.toString(maxLine).length();
-		        // Width of number character is half the height in monospaced font, add 1 character width for right padding.
-		        int bulletWidth = (bulletLength + 1) * styledText.getLineHeight() / 2;
-		        styleRange.metrics = new GlyphMetrics(0, 0, bulletWidth);
-		        event.bullet = new Bullet(ST.BULLET_TEXT, styleRange);
-		        // getLineAtOffset() returns a zero-based line index.
-		        int bulletLine = styledText.getLineAtOffset(event.lineOffset) + 1;
-		        event.bullet.text = String.format("%" + bulletLength + "s", bulletLine);
-		    }
+			@Override
+			public void lineGetStyle(LineStyleEvent event) {
+				// Using ST.BULLET_NUMBER sometimes results in weird alignment.
+				// event.bulletIndex = styledText.getLineAtOffset(event.lineOffset);
+				StyleRange styleRange = new StyleRange();
+				styleRange.foreground = Display.getCurrent().getSystemColor(SWT.COLOR_GRAY);
+				int maxLine = styledText.getLineCount();
+				int bulletLength = Integer.toString(maxLine).length();
+				// Width of number character is half the height in monospaced font, add 1
+				// character width for right padding.
+				int bulletWidth = (bulletLength + 1) * styledText.getLineHeight() / 2;
+				styleRange.metrics = new GlyphMetrics(0, 0, bulletWidth);
+				event.bullet = new Bullet(ST.BULLET_TEXT, styleRange);
+				// getLineAtOffset() returns a zero-based line index.
+				int bulletLine = styledText.getLineAtOffset(event.lineOffset) + 1;
+				event.bullet.text = String.format("%" + bulletLength + "s", bulletLine);
+			}
 		});
 		styledText.addModifyListener(new ModifyListener() {
 			@Override
@@ -92,7 +144,7 @@ public class FrmMain {
 				styledText.redraw();
 			}
 		});
-		
+
 	}
 
 }
