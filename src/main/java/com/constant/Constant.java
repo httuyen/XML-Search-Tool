bb465390696193cf5788c0dd31c5bf4c986aa0c3
Tuyen Huynh <47501532+httuyen@users.noreply.github.com>
2019-09-23 19:32:18 +0700

    23/09/2019

--- src/main/java/com/constant/Constant.java
+++ src/main/java/com/constant/Constant.java
@@ -1,6 +1,12 @@
 package com.constant;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Constant {
 	public static String SELECTED ="";
 	public static String TEXT = "";
+	public static Map<String , Object> childMap1 = new HashMap<String, Object>();
+	//Map<String , Object> childMap1 = new HashMap<String, Object>();
+	public static boolean KEY_FLAG = false;
 }
--- src/main/java/com/process/NodeType.java
+++ src/main/java/com/process/NodeType.java
@@ -1,12 +1,13 @@
 package com.process;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.apache.xerces.parsers.DOMParser;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -17,86 +18,102 @@ import org.xml.sax.SAXException;
 
 import com.constant.Constant;
 
-public class NodeType extends Constant{
-	public static void parseXML(String pathXML) throws ParserConfigurationException, SAXException, IOException {
+public class NodeType extends Constant {
+
+	public static void parseXML(String pathXML)
+			throws ParserConfigurationException, SAXException, IOException, InterruptedException {
+		Map<String, Object> rootMap = new HashMap<String, Object>();
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setValidating(true);
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document xmlDoc = builder.parse(pathXML);
 		Element root = (Element) xmlDoc.getDocumentElement();
-		processNode(root);
-
+		processNode(root, rootMap);
+//		System.out.println(rootMap);
+//		System.out.println(rootMap.containsKey("project_17"));
+		System.out.println(rootMap.get("project_17"));
+		
 	}
 
-	private static void processNode(Element element) {
-		TEXT = TEXT + "<" + element.getNodeName() + "\n";
-		print("<" + element.getNodeName());
-		// Lay ra danh sach cac thuoc tinh cua Element element
+	private static void processNode(Element element, Map<String, Object> rootMap)
+			throws InterruptedException {
+		Map<String, Object> childMap = new HashMap<String, Object>();
+		NodeList nodeList = element.getChildNodes();
+		int count=0;
+		for (int i = 0; i < nodeList.getLength(); i++) {
+			Node node = nodeList.item(i);
+			short type = node.getNodeType();			
+			switch (type) {
+			case Node.ELEMENT_NODE:
+				processNode((Element) node, childMap);// goi method nay cho Element
+				count = i;
+				break;
+			case Node.COMMENT_NODE:
+				break;
+			case Node.TEXT_NODE:
+				break;
+			case Node.ATTRIBUTE_NODE:
+				break;
+			default:
+				break;
+			}
+		}
+		rootMap.put(element.getNodeName()+"_"+count, childMap);
+		System.out.println(rootMap);
+	}
+	
+	@SuppressWarnings("unused")
+	private static void processNode1(Element element) {
+		System.out.println(element.getNodeName());
 		NamedNodeMap nodeMap = element.getAttributes();
 		for (int i = 0; i < nodeMap.getLength(); i++) {
 			// Lay ra nut thu i trong tap hop
 			Attr at = (Attr) nodeMap.item(i);
-			TEXT = TEXT + " "+ at.getNodeName() + "=\"" + at.getNodeValue() + "\"";
+			TEXT = TEXT + " " + at.getNodeName() + "=\"" + at.getNodeValue() + "\"";
 			print("\n\t" + at.getNodeName() + "=\"" + at.getNodeValue() + "\"");
 		}
-		TEXT += ">"+ "\n";
-		print(">"  + "\n");
+		TEXT += ">" + "\n";
+		print(">" + "\n");
 		// Lay ra danh sach cac nut con cua nut Element element
 		NodeList nodeList = element.getChildNodes();
-		//System.out.println(nodeList.getLength());
+		// System.out.println(nodeList.getLength());
 		for (int i = 0; i < nodeList.getLength(); i++) {
 			System.out.println("----------------------" + i + "-------------------------");
 			Node node = nodeList.item(i);
 			short type = node.getNodeType();
-			System.out.println("@@@@@@@@@@@@@@@@@@  "+type+"  @@@@@@@@@@@@@@@@@");
+			System.out.println("@@@@@@@@@@@@@@@@@@  " + type + "  @@@@@@@@@@@@@@@@@");
 			switch (type) {
 			// Truong hop nut con thu i la mot Element
 			case Node.ELEMENT_NODE:
-				processNode((Element) node);// goi method nay cho Element
+				// processNode((Element) node);// goi method nay cho Element
 				break;
 			case Node.COMMENT_NODE:
 				print("<!--" + node.getNodeValue() + "-->\n");
-				
+
 				break;
 			case Node.TEXT_NODE:
-				print(node.getNodeValue()+"\n");
+				print(node.getNodeValue() + "\n");
 				break;
 			case Node.ATTRIBUTE_NODE:
-				print(node.getNodeValue()+"\n");
+				print(node.getNodeValue() + "\n");
 				break;
 			default:
-				//print("Other node.....\n");
+				// print("Other node.....\n");
 				break;
 			}
 		}
 	}
-	
-	private static void processNode1() {
-		String xmlFile = "C:\\apache-maven-3.3.9-bin\\apache-maven-3.3.9\\conf\\settings.xml";
-		DOMParser parser = new DOMParser();
-		try {
-		   //Phân tích tài liệu xml .Sau bước này có một cây DOM trong bộ nhớ
-		   // chính là hình ảnh của tài liệu
-		   parser.parse(xmlFile);
-		} catch (SAXException se) {
-		   se.printStackTrace();
-		} catch (IOException ioe) {
-		   ioe.printStackTrace();
-		}
-		//Lấy ra nút Document mô tả toàn bộ tài liệu XML
-		Document document = parser.getDocument();
-		System.out.println(document.toString());
-	}
-	
+
 	private static void print(String s) {
 		System.out.print(s);
 	}
 
 	/** Ham main */
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		NodeType f = new NodeType();
 		try {
-			f.parseXML("C:\\apache-maven-3.3.9-bin\\apache-maven-3.3.9\\conf\\settings.xml");
+			f.parseXML("D:\\EclipseWorkspace\\SearchTool\\java-project\\pom.xml");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
